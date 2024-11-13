3e761a5fda6bde3788260daa7b2c6ebfc618f422
danielsamaniego370 <danielsamaniego370@gmail.com>
2024-11-13 17:04:00 +0000

    first commit

---
+++ src/app/Components/botones/btn-oscuro/btn-oscuro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-oscuro',
+  standalone: true,
+  imports: [],
+  templateUrl: './btn-oscuro.component.html',
+  styleUrl: './btn-oscuro.component.css'
+})
+export class BtnOscuroComponent {
+
+}
